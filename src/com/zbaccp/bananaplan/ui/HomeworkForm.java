edfd6d45233bc1fe363c5a28e9b37d6e2aad37a4
bananaplan <bananaplan@126.com>
2017-08-18 15:43:42 +0800

    test

--- src/com/zbaccp/bananaplan/ui/HomeworkForm.java
+++ src/com/zbaccp/bananaplan/ui/HomeworkForm.java
@@ -1,6 +1,9 @@
 package com.zbaccp.bananaplan.ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Created by bananaplan on 2017/8/18.
@@ -9,6 +12,27 @@ public class HomeworkForm {
     private static JFrame frame;
 
     private JPanel panelMain;
+    private JButton button1;
+
+    public HomeworkForm() {
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser jfc = new JFileChooser();
+                jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                jfc.showDialog(new JLabel(), "选择");
+                File file = jfc.getSelectedFile();
+                if (file != null) {
+                    if (file.isDirectory()) {
+                        System.out.println("文件夹:" + file.getAbsolutePath());
+                    } else if (file.isFile()) {
+                        System.out.println("文件:" + file.getAbsolutePath());
+                    }
+                }
+            }
+        });
+    }
 
     public static void show() {
         if (frame == null) {
